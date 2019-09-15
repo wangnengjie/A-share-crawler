0541289cc7a10d9aaba37094ffafdf0afd8c2610
wangnengjie <751614701@qq.com>
2019-09-15 14:39:23 +0800

    add RDWR

--- modules/logger.go
+++ modules/logger.go
@@ -6,25 +6,18 @@ import (
 	"os"
 )
 
-var (
-	ERROR_CHANNEL chan string
-	INFO_CHANNEL  chan string
-)
-
-func init() {
-	ERROR_CHANNEL = make(chan string, 100)
-	INFO_CHANNEL = make(chan string, 100)
-}
+var ERROR_CHANNEL = make(chan string, 100)
+var INFO_CHANNEL = make(chan string, 100)
 
 func StartLog() {
-	logFile, err := os.OpenFile("./run.log", os.O_CREATE|os.O_APPEND, os.ModePerm)
+	logFile, err := os.OpenFile("./run.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 	defer logFile.Close()
 	log.SetOutput(logFile)
-	log.SetFlags(log.LstdFlags | log.Llongfile)
+	log.SetFlags(log.LstdFlags)
 	for {
 		select {
 		case val := <-ERROR_CHANNEL:
